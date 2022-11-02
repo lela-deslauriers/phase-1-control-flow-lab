ad38952a32b08c69d44f34431d7779045f22e6d2
Lela Deslauriers <lela.deslauriers@gmail.com>
2022-11-02 14:14:55 -0700

    commit tree 61a8b71dc11c90ccdc623e89fb6c40bee5da9713

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet) {
+  let message; // Call variable
+  if (feet <= 400) {
+    return 'This one is on me!';
+  }
+  else if (feet <= 2000) {
+    return 'That will be twenty bucks.';
+  }
+  else if (feet <= 2500) {
+    return 'I will gladly take your thirty bucks.';
+  }
+  else {
+    return 'No can do.';
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city) {
+  let cityMessage;
+  cityMessage = city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+  return cityMessage;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  console.log(tip);
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+      break;
+    case 'not as generous':
+      return 'Thank you.';
+      break;
+    default:
+      return 'Bye.';
+  }
+
 }
